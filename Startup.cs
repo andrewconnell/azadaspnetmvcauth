da059e63bc869ace7473857372f20acae275893d
Andrew Connell <me@andrewconnell.com>
2014-12-17 06:55:24 -0500

    added all ADAL & OWIN code to plug authentication into the MVC pipeline

---
+++ Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SampleMvcAzAuth.Startup))]
+
+namespace SampleMvcAzAuth {
+  public partial class Startup {
+    public void Configuration(IAppBuilder app)
+    {
+      ConfigureAuth(app);
+    }
+  }
+}
